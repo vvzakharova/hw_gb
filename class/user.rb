d93e68978213e193b22e40f5b39ac8280e141a2c
vvzakharova <85403796+vvzakharova@users.noreply.github.com>
2021-06-14 20:22:53 +0500

    Add files via upload
    Вынесение класса в отдельный файл

---
+++ class/user.rb
@@ -0,0 +1,11 @@
+class User
+
+  def fio
+    'Захарова Виктория'
+  end
+
+  def profession
+    'Химик'
+  end
+
+end
---
+++ class/user_use.rb
@@ -0,0 +1,5 @@
+require_relative 'user'
+
+user = User.new
+p 'ФИО пользователя: ' + user.fio 
+p 'Профессия: ' + user.profession
